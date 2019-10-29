49ef6b95773a57e1fe68cd456c48da0424d50c28
Kobi Cummings <kobicummings@gmail.com>
2019-10-29 09:52:28 -0700

    Updates

---
+++ precache-manifest.41e6a7cb83be965c9b6a263c53f760c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bdc3b0c59a070019e90151c2c81cb7",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "da312f612f0ceb83ecbd",
+    "url": "/clicky_game/static/css/main.93a25e7a.chunk.css"
+  },
+  {
+    "revision": "41e894de3cb430f8c5b0",
+    "url": "/clicky_game/static/js/2.15b3e017.chunk.js"
+  },
+  {
+    "revision": "da312f612f0ceb83ecbd",
+    "url": "/clicky_game/static/js/main.4ee04e2e.chunk.js"
+  },
+  {
+    "revision": "f40d2958c8a8955f1d35",
+    "url": "/clicky_game/static/js/runtime-main.c6ec18fb.js"
+  }
+]);
